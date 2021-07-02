314b882eb203756a5ae44475abfc92e3295ccbd6
Cannon <teamnapinfo@gmail.com>
2021-07-02 23:20:32 +0100

    remove copy pasta from docs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import AeroClient from "@aeroware/aeroclient";
 import { config } from "dotenv";
 config({ path: "src/.env" })
 
-// create a client with default settings and commands
 const client = new AeroClient({
     token: process.env.TOKEN,
     prefix: "!",
