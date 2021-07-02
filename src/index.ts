b062e37ee521517d1ed8544e70a19cf28332af92
Cannon <teamnapinfo@gmail.com>
2021-07-02 23:16:43 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import AeroClient from "@aeroware/aeroclient";
+import { config } from "dotenv";
+config({ path: "src/.env" })
 
 // create a client with default settings and commands
 const client = new AeroClient({
