ec3ad525eccbebbd3301d5a0b5f8fd3bdad7b3b9
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-07-02 20:06:55 -0400

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import AeroClient from "@aeroware/aeroclient";
+import "dotenv/config";
+
+const client = new AeroClient({
+    token: process.env.TOKEN!,
+    prefix: "/",
+    logging: true,
+    loggerHeader: "arcade",
+    commandsPath: "commands",
+    eventsPath: "events",
+    connectionUri: process.env.MONGODB_URI!,
+    useDefaults: true,
+    persistentCooldowns: true,
+    disableStaffCooldowns: true,
+    staff: process.env.STAFF_IDS!.split(/,\s*/g),
+    dev:
+        process.env.NODE_ENV === "development"
+            ? {
+                  eval: {
+                      console: true,
+                      command: true,
+                  },
+              }
+            : undefined,
+});
